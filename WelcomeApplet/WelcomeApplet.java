58f35175e19a4fd1fc052db902a7d1e46aa9f632
David <David@jdsu.com>
2016-12-12 01:53:08 +0800

    Add prj for chapter 2.

---
+++ WelcomeApplet/WelcomeApplet.java
@@ -0,0 +1,57 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.net.*;
+import javax.swing.*;
+
+/**
+* This applet displays a greeting from the authors.
+* @version 1.22 2007-04-08
+* @author Cay Horsmann
+*/
+
+public class WelcomeApplet extends JApplet
+{
+    public void int()
+    {
+//        EventQueue.invokeLater(new Runnable()
+//        {
+//            public void run()
+//            {
+//                setLayout(new BorderLayout());
+//                JLabel label = new jLabel(getParameter("greeting"), SwingConstants.CENTER);
+//                label.setFont(new Font("Serif", Font.BOLD, 18));
+//                add(label, BorderLayout.CENTER);
+//
+//                JPanel panel = new JPanel();
+//
+//                JButton cayButton = new JButton("Cay Horstmann");
+//                cayButton.addActionListener(makeAction("http://www.horstmann.com"));
+//                panel.add(cayButton);
+//
+//                JButton garyButton = new JButton("Gary cornell");
+//                garyButton.addActionListener(makeAction("mailto:gary_corenell@apress.com");
+//                panel.add(garyButton);
+//
+//                add(panel.BorderLayout.SOUTH);
+//            }
+//        });
+    }
+
+//    private ActionListener makeAction(final String urlString)
+//    {
+//        return new ActionListener()
+//        {
+//            public void actionPerformed(ActionEvent event)
+//            {
+//                try
+//                {
+//                    getAppletContext().showDocument(new URL(urlString);
+//                }
+//                catch(MalformedURLException e)
+//                {
+//                    e.printStackTrace();
+//                }
+//            }
+//        };
+//    }
+}
